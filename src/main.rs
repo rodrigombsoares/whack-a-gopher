a22e1109a9a0b6ba99404b301294897c41923ce5
rodrigombsoares <rodrigombsoares@gmail.com>
2024-02-20 19:08:42 -0300

    draw holes

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
 use macroquad::prelude::*;
 
+const MARGING: f32  = 100f32;
+const RADIUS: f32  = 50f32;
+
 #[macroquad::main("BasicShapes")]
 async fn main() {
     loop {
-        clear_background(RED);
+        clear_background(WHITE);
 
-        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
+        draw_rectangle(MARGING*0.5f32, MARGING*0.5f32, screen_width()-MARGING, screen_height()-MARGING, GREEN);
+        for i in 0..6 {
+            let n = i as f32;
+            let x = screen_width()*0.5 - 4.0*RADIUS - 20.0 + MARGING+((n-n%2.0)*(RADIUS+10.0));
+            let y = screen_height()*0.5 - RADIUS - 10.0 +(RADIUS+10.0)*2.0*(n%2.0);
 
+            draw_circle(x, y, RADIUS, BROWN);
+        }
         draw_text("IT WORKS!", 20.0, 20.0, 30.0, DARKGRAY);
 
         next_frame().await
