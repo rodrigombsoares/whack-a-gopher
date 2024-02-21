af7fde6453cb77e291b806d37d2f66fd1e3c8c93
rodrigombsoares <rodrigombsoares@gmail.com>
2024-02-21 10:10:59 -0300

    count points

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use macroquad::prelude::*;
 
 const MARGING: f32 = 100f32;
 const RADIUS: f32 = 50f32;
+const HIT_SCORE: i32 = 100;
 
 struct Gopher {
     time_elapsed: f32,
@@ -42,7 +43,7 @@ impl Hole {
         }
     }
 
-    pub fn update(&mut self, dt: f32, gophers_count: &mut i32, last_gopher_spawned: &mut f64) {
+    pub fn update(&mut self, dt: f32, gophers_count: &mut i32, last_gopher_spawned: &mut f64, points: &mut i32) {
         let key_map = HashMap::from([
             (0, KeyCode::Q),
             (1, KeyCode::A),
@@ -52,9 +53,11 @@ impl Hole {
             (5, KeyCode::D),
         ]);
     
-        // If the hole is empty and last gopher spawned within more than 0.5s add a gopher with 50% probability
+        // Spawn gophers
         let time_since_last_gopher = get_time() - *last_gopher_spawned;
         if self.gopher.is_none() && *gophers_count < 3 && time_since_last_gopher > 0.5{
+            // If the hole is empty and last gopher spawned within more than 0.5s 
+            // add a gopher with 50% probability
             let random: f32 = thread_rng().gen();
             if random > 0.5 {
                 *gophers_count += 1;
@@ -77,6 +80,7 @@ impl Hole {
         if self.is_pressed && self.gopher.is_some() {
             self.gopher=None;
             *gophers_count -= 1;
+            *points += HIT_SCORE;
         }
     }
 
@@ -97,6 +101,7 @@ async fn main() {
     let mut holes: Vec<Hole> = Vec::new();
     let mut gophers_count = 0;
     let mut last_gopher_spawned = 0.0;
+    let mut points = 0;
 
     for i in 0..6 {
         let hole = Hole::new(i as f32);
@@ -105,10 +110,12 @@ async fn main() {
 
     loop {
         clear_background(WHITE);
-        draw_rectangle(MARGING*0.5f32, MARGING*0.5f32, screen_width()-MARGING, screen_height()-MARGING, GREEN);
-        draw_text("IT WORKS!", 20.0, 20.0, 30.0, DARKGRAY);
+        draw_rectangle(1.5*MARGING*0.5f32, 1.5*MARGING*0.5f32, screen_width()-1.5*MARGING, screen_height()-1.5*MARGING, GREEN);
+        draw_text("WHACK-A_GOPHER", 20.0, 20.0, 30.0, DARKGRAY);
+        let score_text = format!("Score: {}!", points);
+        draw_text(&score_text, 20.0, 55.0, 30.0, DARKGRAY);
         for hole in &mut holes {
-            hole.update(get_frame_time(), &mut gophers_count, &mut last_gopher_spawned);
+            hole.update(get_frame_time(), &mut gophers_count, &mut last_gopher_spawned, &mut points);
             hole.draw();
         }
         next_frame().await
